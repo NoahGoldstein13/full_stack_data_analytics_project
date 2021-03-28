e98f7f681ffac96467012446a9c27b6def3b60cd
perry.reynolds <perry.reynolds@icloud.com>
2021-03-28 19:52:03 -0400

    various updates

--- Perry/js/heatmap.js
+++ Perry/js/heatmap.js
@@ -2,7 +2,7 @@ var myMap = L.map("heatmap", {
     center: [37.0902, -95.7129],
     zoom: 13
   });
-  
+
   L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
@@ -14,20 +14,23 @@ var myMap = L.map("heatmap", {
   
   var url = "http://127.0.0.1:5000//api/v1.0/all_data";
   
-  d3.json(url, function(response) {
-  
+  d3.json(url).then(function(response) {
+
     console.log(response);
   
     var heatArray = [];
   
     for (var i = 0; i < response.length; i++) {
-      var location = response[i].location;
-  
-      if (location) {
-        heatArray.push([location.coordinates[1], location.coordinates[0]]);
+      var lat = response[i].Latitude;
+      var lng = response[i].Longitude;
+      //console.log(lat);
+      //console.log(lng);
+      if (lat !== null && lng !== null) {
+        heatArray.push(lat, lng);
       }
     }
-  
+    console.log(heatArray);
+    
     var heat = L.heatLayer(heatArray, {
       radius: 20,
       blur: 35
