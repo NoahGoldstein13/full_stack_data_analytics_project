1eca59ede5697c5f907e8031fa3093495050a0b2
perry.reynolds <perry.reynolds@icloud.com>
2021-03-28 20:58:32 -0400

    Update heatmap.js

--- Perry/js/heatmap.js
+++ Perry/js/heatmap.js
@@ -3,62 +3,60 @@ var myMap = L.map("heatmap", {
     zoom: 13
   });
 
-  function buildHeatmap(care) {
-
-
-
-
-
-
-
-
-  }
-  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
     maxZoom: 18,
     zoomOffset: -1,
     id: "mapbox/streets-v11",
     accessToken: API_KEY
-  }).addTo(myMap);
+}).addTo(myMap);
   
-  var url = "http://127.0.0.1:5000//api/v1.0/all_data";
-  
-  d3.json(url).then(function(response) {
-
-    console.log(response);
-  
-    var heatArray = [];
-  
-    for (var i = 0; i < response.length; i++) {
-      var lat = response[i].Latitude;
-      var lng = response[i].Longitude;
+function buildHeatmap(care) {
     
-      if (lat !== null && lng !== null) {
-        heatArray.push([lat, lng]);
-      }
-    }
-    //console.log(heatArray);
+    var url = "http://127.0.0.1:5000//api/v1.0/all_data";
+    
+    d3.json(url).then(function(response) {
 
-    var heat = L.heatLayer(heatArray, {
-      radius: 20,
-      blur: 35
-    }).addTo(myMap);
+        var care_array = response.filter(datapoint => 
+            
+            datapoint["Value Code"] == care 
+            );
 
-  });
+        //console.log(care_array);
+    
+        var heatArray = [];
+    
+        for (var i = 0; i < care_array.length; i++) {
+        var lat = care_array[i].Latitude;
+        var lng = care_array[i].Longitude;
+        
+        if (lat !== null && lng !== null) {
+            heatArray.push([lat, lng]);
+        }
+        }
+        console.log(heatArray);
 
-  function init() {
+        var heat = L.heatLayer(heatArray, {
+        radius: 20,
+        blur: 35
+        }).addTo(myMap);
+
+    });
+};
+
+function init() {
 
     var selector = d3.select("#selDataset");
 
     const firstSample = "AMI";
     buildHeatmap(firstSample)
-  };
-  
-    // Event Listener
-    function optionChanged(newCareType) {
+};
 
-        buildHeatmap(newCareType)
-    };
+// Event Listener
+function optionChanged(newCareType) {
+
+    buildHeatmap(newCareType)
+};
   
-  init();
+init();
